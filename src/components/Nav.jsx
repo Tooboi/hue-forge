ad256d0db101ef573401f89de1308bd8557c1d17
Tooboi <japica123@gmail.com>
2023-04-14 03:07:24 -0400

    Added tailwind

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+
+const Nav = () => {
+
+  return (
+    <header className="bg-stone-500 h-16">
+    <p className="text-3xl text-center ">HueForge</p>
+
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Nav from '../components/Nav.jsx'
+const Home = () => {
+
+  return (
+    <main className="bg-stone-900 h-screen">
+        <Nav />
+    </main>
+  );
+};
+
+export default Home;
