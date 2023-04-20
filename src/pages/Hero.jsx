358e7839065d1836e2750af22f25fde982298a35
Tooboi <japica123@gmail.com>
2023-04-20 03:07:15 -0400

    Hero Forming

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,57 +1,15 @@
-import React, { useState, useEffect } from 'react';
-// import Overlay from '../components/Overlay'
-// import HueCube from '../assets/HueCube4.webm';
-import Scroll from '../components/Scroll';
-import SaturationValuePicker from '../components/SaturationValuePicker';
+import HueCube from '../assets/HueCubeA2.webm';
 
 const Hero = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const [hue, setHue] = useState(0);
-  const [saturation, setSaturation] = useState(100);
-  const [value, setValue] = useState(50);
-
-  const handleSVChange = (newSaturation, newValue) => {
-    setSaturation(newSaturation);
-    setValue(newValue);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.pageYOffset;
-      setScrollPosition(position);
-      console.log(position);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Clean up the event listener when component unmounts
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const images = [];
-
-  for (let i = 1; i <= 250; i++) {
-    const fileName = i.toString().padStart(4, '0') + '.webp';
-    images.push(require(`../assets/CubeHueWEBP/${fileName}`).default);
-  }
-
-  const currentFrame = Math.floor(scrollPosition / 10); // divide by 10 to get a frame rate of 10 frames per 1px of scroll
-
   return (
-    <div className="bg-stone-950 flex justify-center">
-      {/* <video src={HueCube} autoPlay loop muted /> */}
-      <SaturationValuePicker hue={hue} saturation={saturation} value={value} onSVChange={handleSVChange} />
-      {/* <img src={images[currentFrame]} alt={`Image ${currentFrame}`} /> */}
-      <div className="flex ">
-        {/* <div className="container mx-auto">
-      <img
-        className="mx-auto"
-        src={images[currentImageIndex]}
-        alt="animation"
-      />
-    </div> */}
-        {/* <Scroll /> */}
+    <div className="bg-[#000] flex flex-col justify-center overflow-hidden">
+      <div className='to-black'>
+        <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-amber-500 via-pink-500 to-amber-500 text-center text-6xl py-12 animate-background">Welcome to HueForge</h1>
+      </div>
+      <video src={HueCube} autoPlay loop muted className="w-5/6 m-auto max-w-xl " />
+      <div className="mx-auto flex flex-col justify-center from-black w-full py-12">
+        <h2 className="text-white text-center text-4xl py-12">forge the perfect palette</h2>
+        <button className="text-white w-48 bg-stone-400 mx-auto">Enter</button>
       </div>
     </div>
   );
